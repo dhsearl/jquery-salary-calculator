b8b8834ef4ea4a953bbf96fef703af7cde91f027
David Searl <dhsearl@gmail.com>
2019-10-04 17:22:09 -0500

    wire up the submit button, correctly format numbers, and clears input

--- client.js
+++ client.js
@@ -1,29 +1,77 @@
 let employeeArray = [];
-let totalMontly = 0;
+let totalAnnualy = 0;
 
-function makeEmployee(first, last, id, title, salary){
-   return{ 
-    first: first,
-    last: last,
-    id: id,
-    title: title,
-    salary: Number(salary.replace(/[^0-9]/,''))
-    }
-}
+
 
 $(onReady);
 
 function onReady() {
-    $('#addEmployeeButton').on('click', addEmployee);
+    $('#addEmployeeButton').on('click', submitEvent);
+    updateTotalMontly();
+    $('tbody').on('click','.button',deleteEvent);
 }
 
-function addEmployee() {
+function addToArray() {
     let first = $('#firstName').val();
     let last = $('#lastName').val();
-    let ID = $('#ID').val();
+    let ID = $('#idNum').val();
     let title = $('#title').val();
     let salary = $('#annualSalary').val();
+    let newPerson = makeEmployee(first, last, ID, title, salary);
+    employeeArray.push(newPerson);
 
-    newPerson = makeEmployee(first,last,ID,title,salary);
-    totalMonthly += newPerson.salary;
+    console.log(employeeArray);
+    totalAnnualy += newPerson.salary;
+    return newPerson;
+}
+function makeEmployee(first, last, id, title, salary) {
+    return {
+        first: first,
+        last: last,
+        id: id,
+        title: title,
+        salary: Number(salary.replace(/[^0-9\.]+/g, '')) // Thank you Stack Overflow
+    }
+}
+
+function submitEvent() {
+    let newPerson = addToArray();
+    console.log(newPerson); // For testing
+    $('tbody').append(`
+    <tr>
+    <td>${newPerson.first}</td>
+    <td>${newPerson.last}</td>
+    <td>${newPerson.id}</td>
+    <td>${newPerson.title}</td>
+    <td>${'$ ' + newPerson.salary}</td>
+    <td class="deleteCell"><button class="deleteButton">Delete</button></td>
+</tr>
+`);
+    updateTotalMontly();
+    clearInputs();
+
+}
+
+function updateTotalMontly() {
+    let totalMonthly = turnIntoNumberString(totalAnnualy / 12);
+    $('.salaryTotalArea').empty();
+    $('.salaryTotalArea').append(`<p>Total Monthly: ${totalMonthly}</p>`);
+
+}
+
+// This was taken from Stack Overflow.
+// While I was tempted to use the RegEx 
+//
+function turnIntoNumberString(numberToFormat) {
+    let formatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', });
+    console.log(formatter.format(numberToFormat));
+    return formatter.format(numberToFormat);
+}
+
+function clearInputs() {
+    first = $('#firstName').val("");
+    last = $('#lastName').val("");
+    ID = $('#idNum').val("");
+    title = $('#title').val("");
+    salary = $('#annualSalary').val("");
 }
